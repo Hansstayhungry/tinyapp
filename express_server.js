2162445fb9cccf2303b54c86d7a414e9db72baf1
Hans Wang <hansmikaa@live.cn>
2023-04-17 07:52:50 -0400

    add _header in the partials

--- express_server.js
+++ express_server.js
@@ -14,8 +14,9 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+// :id - user input lookup
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: req.params.id.longURL };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 })
 
